25fdc9666fbd85ee7d98fedfa3d37d53c513ba0c
Desento <astapenko91@gmail.com>
2024-01-30 19:11:40 +0100

    Project initiation

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,8 +6,9 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.js', '**.svg', '**.css'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
