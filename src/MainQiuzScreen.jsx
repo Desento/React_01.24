ae270b47d58cf5dbd389d262439429135bb3b4db
Desento <astapenko91@gmail.com>
2024-02-01 23:21:08 +0100

    refactoring by lint

--- src/MainQiuzScreen.jsx
+++ src/MainQiuzScreen.jsx
@@ -1,37 +1,36 @@
-import { question } from "./content/content.jsx"
-import { QuizQuestion } from "./components/QuizQuestion.jsx";
+import { question } from './content/content.jsx'
+import { QuizQuestion } from './components/QuizQuestion.jsx'
 import './MainQiuzScreen.css'
-import { useState } from "react";
+import { useState } from 'react'
 
 export const MainQuizScreen = () => {
-    const handleAnswer = (answer) => {
-        console.log(`Selected answer: ${answer}`);
-    };
+  const handleAnswer = (answer) => {
+    console.log(`Selected answer: ${answer}`)
+  }
 
-    const handleEndQuiz = () => {
-        console.log('Quiz ended');
-    };
+  const handleEndQuiz = () => {
+    console.log('Quiz ended')
+  }
 
-    const [counterOfQuestions, setCounterOfQuestions] = useState(0);
+  const [counterOfQuestions, setCounterOfQuestions] = useState(0)
 
-    const handleNextQuestion = () => {
+  const handleNextQuestion = () => {
+    setCounterOfQuestions((prevCounter) => prevCounter + 1)
+  }
 
-        setCounterOfQuestions(prevCounter => prevCounter + 1);
-    };
-
-    return (
-        <div className="container">
-            {counterOfQuestions < question.results.length ? (
-                <QuizQuestion
-                    question={question.results[counterOfQuestions]}
-                    totalQuestions={question.results.length}
-                    onAnswer={handleAnswer}
-                    onEndQuiz={handleEndQuiz}
-                    onNextQuestion={handleNextQuestion}
-                />
-            ) : (
-                <p>Quiz completed</p>
-            )}
-        </div>
-    );
+  return (
+    <div className="container">
+      {counterOfQuestions < question.results.length ? (
+        <QuizQuestion
+          question={question.results[counterOfQuestions]}
+          totalQuestions={question.results.length}
+          onAnswer={handleAnswer}
+          onEndQuiz={handleEndQuiz}
+          onNextQuestion={handleNextQuestion}
+        />
+      ) : (
+        <p>Quiz completed</p>
+      )}
+    </div>
+  )
 }
